6d35c43c951e714b29b670988aa4ae1f0e9fd743
Betina Farias <betina.farias@4all.com>
2019-07-08 00:57:50 -0300

    Route tests

--- EventAppTests/EventAppTests.swift
+++ EventAppTests/EventAppTests.swift
@@ -10,25 +10,27 @@ import XCTest
 @testable import EventApp
 
 class EventAppTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGetEventDetailEndpoint() {
+        let route = EventsEndpoint.getEvent(id: "3")
+        XCTAssertEqual(route.method, .GET)
+        XCTAssertNotNil(route.path)
+        XCTAssertEqual(route.path, "/events/3")
+        XCTAssertNil(route.parameters)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testGetEventListEndpoint() {
+        let route = EventsEndpoint.getEvents
+        XCTAssertEqual(route.method, .GET)
+        XCTAssertNotNil(route.path)
+        XCTAssertEqual(route.path, "/events")
+        XCTAssertNil(route.parameters)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testCheckinEndpoint() {
+        let route = CheckinEndpoint.checkin(name: "Teste", email: "teste@teste.com", eventId: "3")
+        XCTAssertEqual(route.method, .POST)
+        XCTAssertNotNil(route.path)
+        XCTAssertEqual(route.path, "/checkin")
+        XCTAssertNotNil(route.parameters)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
